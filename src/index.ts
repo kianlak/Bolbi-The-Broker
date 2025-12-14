caa63cc1981affe044237554f42a2f26e1e20e7a
Kian Lak <lak.kian.ca@gmail.com>
2025-12-13 19:56:23 -0600

    Bare minimum setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-export default {
-  fetch(request: Request) {
-    return new Response("Bolbi the Broker is alive 🦊", {
-      status: 200,
-    });
-  },
-};
+import 'dotenv/config';
+import axios from 'axios';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client(
+  { intents: 
+    [
+      GatewayIntentBits.Guilds, 
+      GatewayIntentBits.GuildMessages, 
+      GatewayIntentBits.MessageContent
+    ]
+  }
+)
+
+client.on('ready', () => {
+  console.log('bot is ready');
+})
