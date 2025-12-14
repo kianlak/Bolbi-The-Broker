2d6f3494e615c7e35bded8e4e23bac9f6a0eb9df
Kian Lak <lak.kian.ca@gmail.com>
2025-12-14 02:39:26 -0600

    Startup and Shutdown Messages Completed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, TextChannel } from 'discord.js';
 
 import { logger } from './shared/logger.ts';
 
 import { commandRouter } from './commandRouter.ts';
+
+const ANNOUNCEMENT_CHANNEL = process.env.DISCORD_ALLOWED_CHANNEL_ID;
 
 const client = new Client({
   intents: [
@@ -16,8 +18,8 @@ const client = new Client({
 
 client.login(process.env.DISCORD_BOT_TOKEN);
 
-client.once('ready', () => {
-  logger.info("Bot is live");
+client.once('ready', async () => {
+  startup();
 });
 
 client.on('messageCreate', async (message) => {
@@ -29,4 +31,56 @@ client.on('messageCreate', async (message) => {
   }
 
   return;
+});
+
+async function startup() {
+  logger.info(`Startup signal received`);
+
+  try {
+    if (!ANNOUNCEMENT_CHANNEL) {
+      logger.error("Announcement Channel could not be found or is invalid");
+      throw new Error();
+    }
+
+    const channel = await client.channels.fetch(ANNOUNCEMENT_CHANNEL);
+
+    if (channel?.isTextBased()) {
+      await (channel as TextChannel).send('`🏪 Bolbi has arrived 🏪`');
+
+      logger.success('Announcement has been sent');
+    }
+  } catch (error) {
+    logger.error(`Failed to send startup message:\n\t${(error as Error).message}`);
+  }
+}
+
+async function shutdown(signal: string) {
+  logger.info(`Shutdown signal received: ${signal}`);
+
+  try {
+    if (!ANNOUNCEMENT_CHANNEL) {
+      logger.error("Announcement Channel could not be found or is invalid");
+      throw new Error();
+    }
+
+    const channel = await client.channels.fetch(ANNOUNCEMENT_CHANNEL);
+
+    if (channel?.isTextBased()) {
+      await (channel as TextChannel).send('`🔕 Bolbi has left his stand 🔕`');
+
+      logger.success('Shutdown message has been sent');
+    }
+  } catch (err) {
+    logger.error(`Failed to send shutdown message:\n\t${(err as Error).message}`);
+  } finally {
+    client.destroy();
+    process.exit(0);
+  }
+}
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+process.on('uncaughtException', async (error) => {
+  logger.error(`Uncaught exception: ${error.message}`);
+  await shutdown('uncaughtException');
 });
